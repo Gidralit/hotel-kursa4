3f1f1a70abe0ed4c2c2f7c8a4aa7c6d8aa13dff5
staskachaev <staskachaev205@mail.ru>
2025-02-28 21:57:27 +0700

    some fix

--- project/frontend/src/components/home-page/HomeGreetingBlock.vue
+++ project/frontend/src/components/home-page/HomeGreetingBlock.vue
@@ -3,7 +3,7 @@
     <div class="greeting__info">
       <h2 class="greeting__title text-4xl">Теперь в отпуске можно не переживать за Барсика</h2>
       <p class="greeting__text text-xl">Мы позаботимся о ваших любимках, пока вас нет рядом!</p>
-      <router-link to="/catalog"><reserved-button class="px-10"/></router-link>
+      <router-link class="w-fit" to="/catalog"><reserved-button class="px-10"/></router-link>
     </div>
   </section>
 </template>
